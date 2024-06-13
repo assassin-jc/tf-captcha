6e00966887faf091a425b297d976812b3dae49ec
assassin-jc <jiangchen_1988@126.com>
2024-06-13 16:56:39 +0800

    调整自建模型结构,添加EfficientNet模型

--- cnnlib/network.py
+++ cnnlib/network.py
@@ -1,3 +1,5 @@
+import keras
+import keras_cv
 import numpy as np
 import tensorflow as tf
 
@@ -13,8 +15,6 @@ class CNN(object):
         self.model_save_dir = model_save_dir  # 模型路径
 
         self.dropout_rate = dropout_rate  # dropout值
-
-
 
     def text2vec(self, text):
         """
@@ -33,58 +33,67 @@ class CNN(object):
             vector[idx] = 1
         return vector
 
+    # 自建模型
     def model(self):
         model = tf.keras.Sequential()  # 构建顺序模型
         # 输入层
-        model.add(tf.keras.Input(shape=(self.image_height, self.image_width, 1)))
+        model.add(tf.keras.Input(shape=(self.image_height, self.image_width, 3)))
 
         # 卷积层1，步长为 1，relu 激活
         model.add(
             tf.keras.layers.Conv2D(
-                filters=32, kernel_size=(3, 3), strides=(1, 1), activation="leaky_relu", padding="SAME"
+                filters=32, kernel_size=(3, 3), strides=(1, 1), padding="SAME"
             )
         )
         model.add(tf.keras.layers.BatchNormalization())
+
+        model.add(tf.keras.layers.Activation("swish"))
         # 最大池化，池化窗口默认为 2
         model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2))
-        # 随机断连
-        model.add(tf.keras.layers.Dropout(rate=self.dropout_rate))
+        # # 随机断连
+        # model.add(tf.keras.layers.Dropout(rate=self.dropout_rate))
 
         # 卷积层2，步为 1，relu 激活
         model.add(
             tf.keras.layers.Conv2D(
-                filters=64, kernel_size=(3, 3), strides=(1, 1), activation="leaky_relu", padding="SAME",
+                filters=64, kernel_size=(3, 3), strides=(1, 1), padding="SAME",
             )
         )
         model.add(tf.keras.layers.BatchNormalization())
+
+        model.add(tf.keras.layers.Activation("swish"))
         # 最大池化，池化窗口默认为 2
         model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2))
-        # 随机断连
-        model.add(tf.keras.layers.Dropout(rate=self.dropout_rate))
+        # # 随机断连
+        # model.add(tf.keras.layers.Dropout(rate=self.dropout_rate))
 
         # 卷积层3，步为 1，relu 激活
         model.add(
             tf.keras.layers.Conv2D(
-                filters=128, kernel_size=(3, 3), strides=(1, 1), activation="leaky_relu", padding="SAME",
+                filters=128, kernel_size=(3, 3), strides=(1, 1), padding="SAME",
             )
         )
         model.add(tf.keras.layers.BatchNormalization())
+
+        model.add(tf.keras.layers.Activation("swish"))
         # 最大池化，池化窗口默认为 2
         model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2))
-        # 随机断连
-        model.add(tf.keras.layers.Dropout(rate=self.dropout_rate))
+        # # 随机断连
+        # model.add(tf.keras.layers.Dropout(rate=self.dropout_rate))
 
         # 卷积层3，步为 1，relu 激活
         model.add(
             tf.keras.layers.Conv2D(
-                filters=256, kernel_size=(3, 3), strides=(1, 1), activation="leaky_relu", padding="SAME",
+                filters=256, kernel_size=(3, 3), strides=(1, 1), padding="SAME",
             )
         )
         model.add(tf.keras.layers.BatchNormalization())
+
+        model.add(tf.keras.layers.Activation("swish"))
         # 最大池化，池化窗口默认为 2
         model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2))
-        # 随机断连
-        model.add(tf.keras.layers.Dropout(rate=self.dropout_rate))
+        # # 随机断连
+        # model.add(tf.keras.layers.Dropout(rate=self.dropout_rate))
 
         # 平均池化
         # model.add(tf.keras.layers.AvgPool2D(pool_size=(3, 3)))
@@ -96,4 +105,20 @@ class CNN(object):
         model.add(tf.keras.layers.Dropout(rate=0.5))
         # 全连接层，Sigmoid 激活
         model.add(tf.keras.layers.Dense(units=self.max_captcha * self.char_set_len, activation="sigmoid"))
+        return model
+
+    # EfficientNet
+    def model_efficientNet(self):
+        # Pretrained backbone
+        # efficientnetv2_b2_imagenet
+        backbone = keras_cv.models.EfficientNetV2Backbone.from_preset('efficientnetv2_s_imagenet')
+        model = keras.Sequential(
+            [
+                keras.layers.Input(shape=(None, None, 3)),
+                backbone,
+                keras.layers.GlobalMaxPooling2D(),
+                keras.layers.Dropout(rate=0.3),
+                keras.layers.Dense(self.max_captcha * self.char_set_len, activation="sigmoid"),
+            ]
+        )
         return model
